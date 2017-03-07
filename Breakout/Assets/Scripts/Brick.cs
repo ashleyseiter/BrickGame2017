601083e1fc2dc6ac4b187811ea5eed812e5369b9
Ashley <ashleyseiter8@gmail.com>
2017-03-07 12:36:37 -0500

    Attempt to add lives to Brick

--- Breakout/Assets/Scripts/Brick.cs
+++ Breakout/Assets/Scripts/Brick.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Brick : MonoBehaviour {
-
+	 
+	int lives = 2;
 	// Use this for initialization
-	void Start () {
-		
+	void start() {
+
 	}
-	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update() {
+		lives = lives - 1;
+		if (lives == 0) {
+			OnCollisionEnter2D (Collision2D coll);
+		}
+	
 	}
 	void OnCollisionEnter2D(Collision2D coll)
 	{
 		gameObject.SetActive(false);
 	}
 }
+
