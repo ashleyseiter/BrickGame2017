6034519a4f7ff6885db37d8396173a0dc72bc3a8
Ashley <ashleyseiter8@gmail.com>
2017-03-09 10:14:26 -0500

    Added Health to bricks

--- Breakout/Assets/Scripts/Brick.cs
+++ Breakout/Assets/Scripts/Brick.cs
@@ -4,22 +4,18 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour {
 	 
-	int lives = 2;
+	public int health = 1;
 	// Use this for initialization
 	void start() {
 
 	}
 	// Update is called once per frame
-	void Update() {
-		lives = lives - 1;
-		if (lives == 0) {
-			OnCollisionEnter2D (Collision2D coll);
-		}
-	
-	}
 	void OnCollisionEnter2D(Collision2D coll)
 	{
-		gameObject.SetActive(false);
+		health -= 1; //	health -= 1 is the same as saying health = health -1;
+		if (health == 0) {
+			gameObject.SetActive (false); // when the collision happens you subract 1 from the health and when health is 0 it dissapears
+		}
 	}
 }
 
